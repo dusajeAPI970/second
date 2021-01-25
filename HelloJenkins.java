c8159d55409b4eae550077fb189d5f77f0d67f4d
dusajeAPI970 <rahul.dusaje@apisero.com>
2021-01-25 20:50:20 +0530

    from branch 2

---
+++ HelloJenkins.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		System.out.println("Hello This is executing in jenkins in branch 2");
+		
+		
+	}
+}
--- HelloJenkinsJenkins.java
+++
@@ -1,9 +0,0 @@
-package demo;
-
-public class HelloJenkinsJenkins {
-
-	public static void main(String[] args) {
-		
-		System.out.println("Hello Jenkins Jenkins");
-	}
-}
